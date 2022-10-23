4b2c6fec31ece82b691dec3a0781de31c0b37725
kjirou <sorenariblog@gmail.com>
2022-10-23 16:03:31 +0900

    wip

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,11 @@
-console.log("cli.js here!");
+const core = require('@actions/core');
+
+try {
+  console.log("Start the core process of issue-based-release-pr.");
+  const baseBranchName = core.getInput('base-branch-name');
+  console.log(`Hello ${baseBranchName}!`);
+  throw new Error('ERROOOORR!!!!!');
+  console.log("Complete the core process of issue-based-release-pr.");
+} catch (error) {
+  core.setFailed(error.message);
+}
