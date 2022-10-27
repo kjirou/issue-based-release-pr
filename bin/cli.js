fa6ace2597565923758a0dec64613cd5cec5ced1
kjirou <sorenariblog@gmail.com>
2022-10-27 09:20:46 +0900

    Remove error throwing to succeed

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,6 @@ const core = require('@actions/core');
 try {
   const baseBranchName = core.getInput('base-branch-name');
   console.log(`Hello ${baseBranchName}!`);
-  throw new Error('ERROR!!!');
 } catch (error) {
   core.setFailed(error.message);
 }
