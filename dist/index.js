2bf64a58cc9348ba36404f4cf001338d8d088707
kjirou <sorenariblog@gmail.com>
2022-10-27 09:22:21 +0900

    Exec `npm run build`

--- dist/index.js
+++ dist/index.js
@@ -2822,7 +2822,6 @@ const core = __nccwpck_require__(186);
 try {
   const baseBranchName = core.getInput('base-branch-name');
   console.log(`Hello ${baseBranchName}!`);
-  throw new Error('ERROR!!!');
 } catch (error) {
   core.setFailed(error.message);
 }
