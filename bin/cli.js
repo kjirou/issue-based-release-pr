f396f661e9fb6149e1f6dff596ee70886f7ad8a3
kjirou <sorenariblog@gmail.com>
2022-10-27 09:12:38 +0900

    Get a param from action

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,9 @@
-console.log("cli.js here!");
+const core = require('@actions/core');
+
+try {
+  const baseBranchName = core.getInput('base-branch-name');
+  console.log(`Hello ${baseBranchName}!`);
+  throw new Error('ERROR!!!');
+} catch (error) {
+  core.setFailed(error.message);
+}
