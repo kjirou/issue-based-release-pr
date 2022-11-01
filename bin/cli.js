6421fab2ada1a89111d34990f0f23b4805d2558c
kjirou <sorenariblog@gmail.com>
2022-11-01 09:40:22 +0900

    No using .mjs
    Because Jest does not consider it as a default setting.

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,8 @@ const core = require("@actions/core");
 // TODO: It's for development, so it's excluded from the build target.
 const parseArgs = require("minimist");
 
+const { main } = require("../lib/main");
+
 const commandArgs = parseArgs(process.argv.slice(2));
 const baseBranchName =
   core.getInput("base-branch-name") || commandArgs["base-branch-name"] || "";
@@ -9,10 +11,10 @@ const githubToken =
   core.getInput("github-token") || commandArgs["github-token"] || "";
 const rawRepo = core.getInput("repo") || commandArgs.repo || "";
 
-import("../lib/main.mjs").then(async ({ main }) => {
+(async () => {
   try {
     await main({ baseBranchName, githubToken, rawRepo });
   } catch (error) {
     core.setFailed(error.message);
   }
-});
+})();
